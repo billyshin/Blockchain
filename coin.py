ed2e8425f5fa2c0e29e911a8cde827224ad79acf
Youngsup Shin <youngsup.shin@mail.utoronto.ca>
2019-01-05 13:14:57 -0500

    Implemented add_transaction request

--- coin.py
+++ coin.py
@@ -140,3 +140,15 @@ def is_valid():
     else:
         response = {'message': 'The Blockchain is not valid.'}
     return jsonify(response), 200
+
+
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods = ['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all(key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201

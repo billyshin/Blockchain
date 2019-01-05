44f85d1fd20d99cd48b07d1260415bc4382da851
Youngsup Shin <youngsup.shin@mail.utoronto.ca>
2019-01-05 13:00:16 -0500

    Fixed typo in blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -10,17 +10,16 @@ from flask import Flask, jsonify
 A class that is used to build the blockchain.
 """
 class Blockchain:
+
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
 
     def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': str(datetime.datetime.now())
-            'proof': proof,
-            'previous_hash': previous_hash
-        }
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
 
@@ -30,19 +29,19 @@ class Blockchain:
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
-        while not check_proof:
-            hash_operation = hashlib.sha256(str(new_proof ** 2 = previous_proof ** 2).encode()).hexdigest()
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof += 1
         return new_proof
-
+    
     def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys=True)
+        encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
-        def is_chain_valid(self, chain):
+    def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
         while block_index < len(chain):
@@ -62,11 +61,12 @@ class Blockchain:
 """ 
 Mining the blockchain
 """
-# Create a Web App
+# Creating a Web App
 app = Flask(__name__)
 
-# Create a Blockchain that we just defined
+# Creating a Blockchain
 blockchain = Blockchain()
+
 
 # Mining a new block
 @app.route('/mine_block', methods = ['GET'])
@@ -76,7 +76,7 @@ def mine_block():
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
-    response = {'message': 'Congratulations, you just mined a block!',
+    response = {'message': 'Mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
@@ -99,9 +99,8 @@ def is_valid():
     if is_valid:
         response = {'message': 'The Blockchain is valid.'}
     else:
-        response = {'message': 'Problem! The Blockchain is not valid.'}
+        response = {'message': 'The Blockchain is not valid.'}
     return jsonify(response), 200
 
-
 # Running the app
-app.run(host = '0.0.0.0', port = 5000)
+app.run(host = '127.0.0.1', port = 5000)
